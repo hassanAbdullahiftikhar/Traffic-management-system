9bbf9555a2ba5bdf5b3fbd29dbcf6e55ad323e7a
hassanAbdullahiftikhar <157296548+hassanAbdullahiftikhar@users.noreply.github.com>
2024-11-23 19:08:43 +0500

    Update weightedgraph.cpp
    weighted directed graph being constructed from csv file.

--- weightedgraph.cpp
+++ weightedgraph.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 using namespace std;
 struct Vertex {
+    char n;
     int a;             
     float weight;      
     Vertex* next;        
@@ -11,6 +15,7 @@ struct Vertex {
     }
     Vertex(int a, float b) {
         this->a = a;
+        n = a + 65;
         this->weight = b;
         next = nullptr;
     }
@@ -54,39 +59,92 @@ public:
 
 class WeightedGraph {
     int n;             
-    List** adjacency_list; 
+    List** adjacency_list;
+    int hash(char c) {
+        return c % 65;
+    }
+    int count_vertex() {
+        fstream read("road_network.csv");
+        int c = 0;
+        string l;
+        bool head = true;
+        while (getline(read, l)) {
+            if (head) {
+                head = false;
+                continue;
+            }
+            c++;
+        }
+        return c;
+    }
+    void load_roads() {
+        fstream read("road_network.csv");
+        string l;
+        bool head = true;
+        while (getline(read, l)) {
+            if (head) {
+                head = false;
+                continue;
+            }
+
+            stringstream ss(l);
+            char vertex, nei;
+            float weight;
+            string token;
+
+            getline(ss, token, ',');
+            vertex = token[0];
+
+            getline(ss, token, ',');
+            nei = token[0];
+
+            getline(ss, token, ',');
+            weight = stof(token);
+
+            int v_idx = vertex - 'A';
+            int n_idx = nei - 'A';
+
+            adjacency_list[v_idx]->insert(n_idx, weight);
+        }
+    }
 public:
-    WeightedGraph(int n) {
-        this->n = n;
+    WeightedGraph() {
+        this->n = count_vertex();
         adjacency_list = new List * [n];
-        cout << "Enter vertices and their neighbors (enter -1 to stop):\n";
         for (int i = 0; i < n; i++) {
             adjacency_list[i] = new List();
-            cout << "Vertex " << i << ":\n";
-            while (true) {
-                int neighbor;
-                float weight;
-                cin >> neighbor;
-                if (neighbor == -1) break;
-                cin >> weight;
-                adjacency_list[i]->insert(neighbor, weight);
-            }
         }
+        load_roads();
     }
     void display() {
         for (int i = 0; i < n; i++) {
-            cout << "Vertex " << i << ": ";
+            char v = 'A';
+            v += i;
+            cout << "Vertex " << v<< ": ";
             Vertex* curr = adjacency_list[i]->getHead();
             while (curr != nullptr) {
-                cout << "(" << curr->a << ", " << curr->weight << ") -> ";
+                cout << "(" << curr->n << ", " << curr->weight << ") -> ";
                 curr = curr->next;
             }
             cout << "NULL\n";
         }
     }
-    /*List dijkstras(int s,int d) {
-        List minpath;
-        Vertex* src = adjacency_list[s]->;
+    /*void dijkstras(char st,char end) {
+        List path;
+        int start = hash(st);
+        int cd = 0;
+        path.insert(adjacency_list[start]->getHead()->a,
+            adjacency_list[start]->getHead()->weight);
+        Vertex* curr = path.getHead();
+        Vertex* p;
+        int d = 999999;
+        while (curr != nullptr) {
+            if (curr->weight < d) {
+                p = curr;
+                d = cd + curr->weight;
+            }
+            curr = curr->next;
+        }
     }*/
     ~WeightedGraph() {
         for (int i = 0; i < n; i++) {
@@ -103,11 +161,7 @@ public:
 };
 
 int main() {
-    int vertices;
-    cout << "Enter the number of vertices: ";
-    cin >> vertices;
-    WeightedGraph graph(vertices);
-    cout << "\nGraph representation:\n";
-    graph.display();
+    WeightedGraph g;
+    g.display();
     return 0;
 }
