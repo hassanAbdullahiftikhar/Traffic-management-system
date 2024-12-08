dd2ca5102c94b2ca4fec28e9d11400d24b5a92f1
hassanAbdullahiftikhar <157296548+hassanAbdullahiftikhar@users.noreply.github.com>
2024-12-08 16:56:24 +0500

    Update congestion.h

--- congestion.h
+++ congestion.h
@@ -14,7 +14,7 @@ struct cong_level {
 };
 
 class congestion_heap {
-    int n;  
+    int n;
     cong_level* cars = nullptr;
     int index = 1;
     int left_c(int i) {
@@ -31,7 +31,7 @@ class congestion_heap {
     void heapify_down() {
         int i = 1;
         while (left_c(i) < index) {
-            int largest = i; 
+            int largest = i;
             int left = left_c(i);
             int right = right_c(i);
             if (left < index && cars[left].no_cars > cars[largest].no_cars) {
@@ -44,7 +44,7 @@ class congestion_heap {
                 cong_level temp = cars[i];
                 cars[i] = cars[largest];
                 cars[largest] = temp;
-                i = largest;  
+                i = largest;
             }
             else {
                 break;
@@ -63,49 +63,51 @@ class congestion_heap {
 
 public:
     congestion_heap() {
-        n = 26;  
-        cars = new cong_level[n + 1];  
+        n = 26;
+        cars = new cong_level[n + 1];
         for (int i = 1; i <= n; i++) {
-            cars[i].no_cars = -1;  
-            cars[i].level = ' ';  
+            cars[i].no_cars = -1;
+            cars[i].level = ' ';
         }
     }
     void decrement_top() {
-        cars[1].no_cars=0;
-		heapify_down();
+        cars[1].no_cars = 0;
+        heapify_down();
     }
     void increment_cars(char s) {
         for (int i = 1; i <= n; i++) {
             if (cars[i].level == s) {
-				cars[i].no_cars++;
+                cars[i].no_cars++;
                 heapify(i);
 
             }
         }
     }
     void remove_top() {
-		cars[0] = cars[index - 1];
+        cars[0] = cars[index - 1];
         cars[index - 1].no_cars = -1;
         cars[index - 1].level = ' ';
-		heapify_down();
+        heapify_down();
 
     }
     void insert_car(char s, int no_cars) {
         cars[index].no_cars = no_cars;
-		cars[index].level = s;
-        heapify(index);  
+        cars[index].level = s;
+        heapify(index);
         index++;
     }
 
     void display_congestion_levels() {
-        cout << "----------------Congestion levels------------------\n";
+        cout << "\n\t\t\t\t\t\t -----------------\n";
+        cout << "\t\t\t\t\t\t|Congestion levels|";
+        cout << "\n\t\t\t\t\t\t -----------------\n\n";
         for (int i = 1; i <= n; i++) {
-            cout << "Intersection point: " << cars[i].level
-                << ", No of cars: " << cars[i].no_cars << "\n";
+            cout << "->Intersection point: " << cars[i].level
+                << " , No of cars: " << cars[i].no_cars << "\n";
         }
     }
 
     cong_level get_max_congestion() {
-        return cars[1];  
+        return cars[1];
     }
 };
